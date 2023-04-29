552ee721ee4f66140f83f3b22ba275ab72019d4e
Julian Camacho <julian.francama@gmail.com>
2023-04-29 13:07:22 -0300

    add type colors

---
+++ src/helpers/typeColors.js
@@ -0,0 +1,41 @@
+const typeColors = {
+    bug: "#26de81",
+    dragon: "#ffeaa7",
+    electric: "#fed330",
+    fairy: "#FF0069",
+    fighting: "#30336b",
+    fire: "#f0932b",
+    flying: "#81ecec",
+    grass: "#00b894",
+    ground: "#EFB549",
+    ghost: "#a55eea",
+    ice: "#74b9ff",
+    normal: "#95afc0",
+    poison: "#6c5ce7",
+    psychic: "#a29bfe",
+    rock: "#2d3436",
+    water: "#0190FF",
+};
+
+export default typeColors;
+
+// const typeColors = {
+//     bug: '#729f3f',
+//     dragon: '#53a4cf',
+//     fairy: '#fdb9e9',
+//     fire: '#fd7d24',
+//     ghost: '#7b62a3',
+//     ground: '#f7de3f',
+//     normal: '#a4acaf',
+//     pyschic: '#f366b9',
+//     steel: '#9eb7b',
+//     dark: '#707070',
+//     electric: '#eed535',
+//     fighting: '#d56723',
+//     flying: '#3dc7ef',
+//     grass: '#9bcc50',
+//     ice: '#51c4e7',
+//     poison: '#b97fc9',
+//     rock: '#a38c21',
+//     water: '#4592c4'
+// }
